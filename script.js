61a890336055d5b6b4db9a25a197eac22ec48507
jino.michel.aque <contact@geeklogbook.com>
2023-07-04 16:17:21 -0300

    general improvements

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
-const project_container = document.getElementById('projects-container')
-
-const projects_count = 50
+const projectContainer = document.getElementById('projects-container');
+const projectsCount = 50;
 
 const fetchProjects = async () => {
-    for (let i = 0; i < projects_count; i++) {
-        await getProjects(i)
-    }
-}
+  for (let i = 0; i < projectsCount; i++) {
+    await getProject(i);
+  }
+};
 
-const getProjects = async (idx) => {
-    const res = await fetch('projects.json')
-    const data = await res.json()
-    createProjectCard(data[idx])
-}
+const getProject = async (idx) => {
+  const response = await fetch('projects.json');
+  const data = await response.json();
+  createProjectCard(data[idx]);
+};
 
 const createProjectCard = (project) => {
-    const projectEl = document.createElement('div')
-    projectEl.classList.add("project")
+  const projectEl = document.createElement('div');
+  projectEl.classList.add('project');
 
-    const id = project.id
-    const name = project.name
-    const location = project.location
-    const image = project.image
-    const url = project.url
+  const { id, name, location, image, url } = project;
 
+  const projectInnerHTML = `
+    <div class="img-container">
+      <img src="${image}" alt="" />
+    </div>
+    <div class="info">
+      <h2>${name}</h2>
+      <p><a href="${url}" target="_blank">Review</a></p>
+    </div>
+    <button>
+      <a href="./projects/${id}.${location}/index.html" class="href" target="_blank">Go to the project</a>
+    </button>
+  `;
+  projectEl.innerHTML = projectInnerHTML;
+  projectContainer.appendChild(projectEl);
+};
 
-    const projectInnerHTML = `
-        <div class="img-container">
-            <img src= ${image} alt="" />
-        </div>
-        <div class="info">
-            <h2>${name}</h2>
-            <p> <a href=${url} target="_blank"> Review </a> </p>
-        </div>
-
-        <button> 
-            <a href="./projects/${id}.${location}/index.html" class="href" target="_blank">Go to the project</a>
-        </button>
-    `
-    projectEl.innerHTML = projectInnerHTML
-    project_container.appendChild(projectEl)
-}
-
-fetchProjects()
+fetchProjects();
